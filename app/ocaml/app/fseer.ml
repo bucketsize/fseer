ae5de5866b305b1d3736bdf3a88f801d13ed9a48
Herr Bucket <bucket.size@gmail.com>
2022-01-29 18:00:06 +0530

    main app

---
+++ app/ocaml/app/fseer.ml
@@ -0,0 +1,25 @@
+open Fseer
+
+let () =
+    let writefn =
+        if Array.length Sys.argv > 1 then
+            match Sys.argv.(1) with
+            | "lemonbar" -> Lemonbar_writer.write
+            | _ -> Console_writer.write
+        else
+            Console_writer.write and
+        () = Sound.info()    
+    in
+    while true do 
+        let sysinfo: Metrics.metrics = {
+            cpuinfo = Cpu.info();
+            cpufreq = Cpufreq.info();
+            cputemp = Cputemp.info();
+            meminfo = Mem.info();
+            netinfo = Net.info();
+            pwrinfo = Power.info();
+        } in
+        writefn sysinfo;
+        flush_all ();
+        Unix.sleep Consts.poll_interval;
+    done
